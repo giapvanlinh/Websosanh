7568882ea7c5c44b023b15a5337b2a72b34f64fc
NgocVB <quanggvn@kaopiz.com>
2020-06-05 12:22:41 +0700

    update 5.6.2020

---
+++ frontend/details.php
@@ -0,0 +1,163 @@
+<?php include('./asset/header.php'); ?>
+
+					<div id="wrap-inner" class="container">
+						<div id="product-info" style="margin-top: 60px;">
+							<div class="clearfix"></div>
+							<h3>Điện Thoại iPhone 11 Quốc Tế Mỹ 64Gb Mới Likenew</h3>
+							<div class="row">
+								<div id="product-img" class="col-xs-12 col-sm-12 col-md-3 text-center">
+									<img src="img/details/iphone_02.jpeg">
+								</div>
+								<div id="product-details" class="col-xs-12 col-sm-12 col-md-9">
+									<p>Giá: <span class="price">14.700.000₫</span></p>
+									<p>- Màn hình 6.1 inch Liquid Retina HD công nghệ True Tone</p> 
+									<p>- Màn hình 6.1 inch Liquid Retina HD công nghệ True Tone<br> - Độ phân giải 1792 x 828 pixels&nbsp;<br> -&nbsp;Camera kép 12MP (ƒ/1.8 góc rộng và ƒ/2.4 siêu rộng) hỗ trợ chế độ Ban đêm<br> - Camera TrueDepth 12MP - ƒ/2.2 quay video 4K lên đến 60fps<br> - Chip A13 Bionic gen 3 Neural Engine<br> - Lưu trữ 64GB<br> - Chuẩn chống nước IP68 (độ sâu 2m tối đa 30 phút)</p>
+									<a href="https://shopee.vn/p-i.25826246.4606651618?deep_and_deferred=1&pid=partnerize_int&af_click_lookback=7d&is_retargeting=true&af_reengagement_window=7d&af_installpostback=false&af_sub2=SHOPEE&clickid=1100l8s58RWV&af_siteid=1011l66585&utm_source=1011l66585&utm_medium=affiliates"><button type="button" class="btn btn-danger" style="width: 400px;">Đến nơi bán</button></a>
+								</div>
+							</div>							
+						</div>
+
+						<div id="comment" style="margin-top: 100px;">
+							<style type="text/css">
+								.clear{
+									clear: both;
+									height: 50px;
+								}
+								.brand {
+									border-radius:50%;
+									-moz-border-radius:50%;
+									-webkit-border-radius:50%;
+								}
+								.price{
+									color: red;
+								}
+								.name-align{
+									float: right;
+									margin-top: 60px;
+								}
+								.products-1{
+									width: 600px;
+									float: left;
+								}
+								.brands-01{
+									width: 300px;
+									float: right;
+								}
+							</style>
+							<h2><b>So sánh giá</b></h2>
+						
+							<div style="height: 100px; margin-top: 100px">
+								<div style="width: 600px; float: left;">
+									<img src="./img/details/sosanh-011.jpeg" alt="pic">
+									<div class="name-align">
+										<p>Điện Thoại iPhone 11 Quốc Tế Mỹ 64Gb Mới Likenew - Shopee &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+									</div>
+								</div>
+								<div class="brands-01">
+									<img class="brand" src="./img/details/shopee-mall.jpg" alt="pic">
+									<div style="float: right;">
+										<p class="price">14.159.000₫</p>
+										<a href="https://shopee.vn/p-i.25826246.4606651618?deep_and_deferred=1&pid=partnerize_int&af_click_lookback=7d&is_retargeting=true&af_reengagement_window=7d&af_installpostback=false&af_sub2=SHOPEE&clickid=1101l8sdsHRF&af_siteid=1011l66585&utm_source=1011l66585&utm_medium=affiliates" target="blank"><button type="button" class="btn btn-success">Đến nơi bán</button></a>
+									</div>
+								</div>
+							</div>
+							<div class="clear"></div>
+							<div style="height: 100px;">
+								<div class="products-1">
+									<img src="./img/iphone_04.jpeg" alt="pic">
+									<div class="name-align">
+										<p>Điện Thoại iPhone 11 64GB - Hàng Nhập Khẩu Chính Hãng - Vàng - Tiki
+										</p>
+									</div>
+								</div>
+								<div class="brands-01">
+									<img class="brand" src="./img/details/tiki.jpg" alt="pic">
+									<div style="float: right;">
+										<p class="price">17.000.000₫</p>
+										<a href="https://tiki.vn/dien-thoai-iphone-11-64gb-hang-nhap-khau-chinh-hang-p32033704.html?utm_campaign=AFF_NBR_ACT_sosanhgia_TI2_ALL_VN_ALL_UNK_UNK_N.1116&utm_medium=d5zxffFg48tDwMYlSHJMjHnv4vrNzpUvpMDlUutE1sTuNp2n&spid=43749335&utm_source=accesstrade&aff_sid=d5zxffFg48tDwMYlSHJMjHnv4vrNzpUvpMDlUutE1sTuNp2n" target="blank"><button type="button" class="btn btn-success">Đến nơi bán</button></a>
+									</div>
+								</div>
+							</div>
+							<div class="clear"></div>
+							<div style="height: 100px;">
+								<div class="products-1">
+									<img src="./img/iphone_04.jpeg" alt="pic">
+									<div class="name-align">
+										<p>Điện Thoại iPhone 11 64GB - Mới 100% Nguyên Seal - Vàng - Lazada &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+									</div>
+								</div>
+								<div class="brands-01">
+									<img class="brand" src="./img/details/lazada.jpeg" alt="pic">
+									<div style="float: right;">
+										<p class="price">17.499.000₫</p>
+										<a href="https://www.lazada.vn/products/dien-thoai-iphone-11-64gb-moi-100-nguyen-seal-vang-i348202368-s566404546.html?laz_trackid=2:mm_150021311_51103013_2010103007:clk5h31dm1ea16tsfe36fn" target="blank"><button type="button" class="btn btn-success">Đến nơi bán</button></a>
+									</div>
+								</div>
+							</div>
+							<div class="clear"></div>
+							<div style="height: 100px;">
+								<div class="products-1">
+									<img src="./img/details/sosanh-015.jpeg" alt="pic">
+									<div class="name-align">
+										<p>Điện thoại Iphone 11 4GB 64GB - Hàng nhập khẩu - IP11 4/64 - Sendo &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+									</div>
+								</div>
+								<div class="brands-01">
+									<img class="brand" src="./img/details/sendo.jpeg" alt="pic">
+									<div style="float: right;">
+										<p class="price">17.690.000₫</p>
+										<a href="https://www.sendo.vn/dien-thoai-iphone-11-4gb-64gb-hang-nhap-khau-23959110.html?utm_source=sosanhgia.com&utm_medium=referral&utm_campaign=datafeed" target="blank"><button type="button" class="btn btn-success">Đến nơi bán</button></a>
+									</div>
+								</div>
+							</div><div class="clear"></div>
+							<div style="height: 100px;">
+								<div class="products-1">
+									<img src="./img/details/sosanh-015.jpeg" alt="pic">
+									<div class="name-align">
+										<p>Apple iPhone 11 1 Sim 64GB - HNam Mobile​​​ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+									</div>
+								</div>
+								<div class="brands-01">
+									<img class="brand" src="./img/details/hnammobile.jpg" alt="pic">
+									<div style="float: right;">
+										<p class="price">17.599.000₫</p>
+										<a href="https://www.hnammobile.com/dien-thoai/apple-iphone-11-1-sim-64gb.17921.html?utm_source=accesstrade&aff_sid=P0k8qVSla47433L9ktmcBuslkIgOC13Dwy96Yk6eHo7cEDJ6" target="blank"><button type="button" class="btn btn-success">Đến nơi bán</button></a>
+									</div>
+								</div>
+							</div>
+
+							
+						</div>
+
+						<div class="clear"></div>
+						<div id="product-detail" style="margin-top: 30px;">
+							<h2><b>Chi tiết sản phẩm</b></h2>
+							<p><b><h5>iPhone 11 được thiết kế mặt kính bóng bẩy, các góc cạnh được bo tròn nhẹ nhàng</h5></b></p>
+							<p>iPhone 11 sở hữu ngoại hình cực kỳ ấn tượng với thiết kế góc cạnh nam tính và 4 góc của máy được bo tròn nhẹ nhằm tạo cảm giác cầm nắm thoải mái hơn cho người dùng. Mặt lưng làm từ chất liệu kính bóng bẩy và đi kèm với các tùy chọn màu sắc khác nhau như xám, bạc, vàng.</p>
+
+							<p>Ở mặt trước, iPhone 11 vẫn sở hữu thiết kế tràn viền ấn tượng như thế hệ trước nhưng chiếc “tai thỏ” giờ đây đã được tối ưu kích thước nên nhỏ gọn hơn. Ngoài ra, viền bezel trên và dưới cũng được cắt gọt đi tối đa giúp thiết bị này gần như không có viền màn hình ở cả 4 cạnh.</p>
+							<p><b><h5>iPhone 11 sử dụng công nghệ màn hình Retina LCD 6.1 inch </h5></b></p>
+							<p>Màn hình Retina 6.1 inch, độ phân giải màn hình 1792 x 828 pixel có độ chính xác màu sắc tốt nhất cho bất kỳ màn hình nào, với độ phản xạ nhỏ và góc nhìn thay đổi màu sắc biến dạng.</p>
+							<p>Đặc biệt, iPhone 11 này còn được Apple trang bị cho màn hình có tần số quét tối đa từ 90 đến 120Hz với 463 điểm màu và tỷ lệ màn hình lên đến 80,2%</p>
+							<p><b><h5>Bộ xử lý Apple A13 trên iPhone 11 giúp tối ưu hiệu năng một cách triệt để</h5></b></p>
+							<p>Hãng sản xuất bán dẫn Đài Loan TSMC sẽ tiếp tục là nhà sản xuất độc quyền cho con chip A13 này. Bộ xử lý Apple A13 sẽ có số model là T8030 và đi kèm với hiệu năng mạnh mẽ cũng như tiết kiệm năng lượng hơn so với thế hệ trước.</p>
+							<p>Được xây dựng trên tiến trình 7nm có mật độ bóng bán dẫn nhiều hơn gấp 70% so với Apple A12 có thể tối ưu hiệu năng một cách triệt để, hỗ trợ tăng hiệu suất và tiết kiệm pin hiệu quả hơn.</p>
+							<p>Ngoài ra, hệ điều hành iOS v13.0 sẽ giúp cho iPhone 11 2019 hoạt động mượt mà hơn rất nhiều: Tốc độ mở ứng dụng tăng 40%, tốc độ hiển thị bàn phím nhanh hơn 50%, và tốc độ truy cập camera cũng được cải thiện đến 70%.</p>
+							<p><b><h5>iPhone 11 sẽ được trang bị Wifi 6 tăng tốc độ kết nối internet tới 40%</h5></b></p>
+							<p>iPhone 11 sẽ hỗ trợ chuẩn Wi-Fi 6 mới nhất, dựa trên công nghệ IEEE 802.11ax. Công nghệ mới nhất này sẽ góp phần tăng tốc độ kết nối Internet qua Wi-Fi nhanh hơn tới 40% so với chuẩn Wi-Fi 5 (802.11ac) ra mắt hồi năm 2014.</p>
+							<p>Wi-Fi 6 là việc hỗ trợ tăng thời lượng pin, điều chắc chắn sẽ khiến nhiều người dùng iPhone hài lòng hơn cả. Tiêu chuẩn Wi-Fi mới cũng sẽ trang bị tính năng "target wake time", về cơ bản sẽ giúp báo chính xác về thời điểm đặt chế độ thu, phát sóng Wi-Fi sang trạng thái ngủ, đồng thời đánh thức thiết bị khi nhận lần truyền phát tiếp theo.</p>
+						</div>
+
+				</div>
+						
+						
+					</div>					
+					<!-- end main -->
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- endmain -->
+
+	<!-- footer -->
+	<?php include('./asset/footer.php'); ?>
---
+++ frontend/index.php
@@ -0,0 +1,149 @@
+<?php 
+	include('./asset/re-header.php');
+ ?>
+		<div id="wrap-inner">
+			<div class="products">
+				<h3>Điện thoại</h3>
+				<div class="product-list row">
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/apple-iphone-11-64gb.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Apple iPhone 11 64GB</a></p>
+						<p class="price">14.700.000₫</p>	  
+						<div class="marsk">
+							<a href="./details.php">Xem chi tiết</a>
+						</div>                                    
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/apple-iphone-11-pro-max-64gb.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Apple Iphone 11 Pro Max 64GB</a></p>
+						<p class="price">25.790.000₫</p>	  
+						<div class="marsk">
+							<a href="./details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/samsung-galaxy-a51-a515.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Samsung Galaxy A51 A515</a></p>
+						<p class="price">5.800.000₫</p>	  
+						<div class="marsk">
+							<a href="./details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/dien-thoai-nokia-6300.jpg" class="img-thumbnail"></a>
+						<p><a href="#">Điện thoại Nokia 6300</a></p>
+						<p class="price">200.000₫</p>	  
+						<div class="marsk">
+							<a href="details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/apple-iphone-11-128gb.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Apple iPhone 11 128GB</a></p>
+						<p class="price">17.699.000₫</p>	  
+						<div class="marsk">
+							<a href="details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/samsung-galaxy-s7-edge.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Samsung Galaxy S7 Edge</a></p>
+						<p class="price">2.980.000₫</p>	  
+						<div class="marsk">
+							<a href="details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/samsung-galaxy-a01.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Samsung Galaxy A01</a></p>
+						<p class="price">1.790.000₫</p>	  
+						<div class="marsk">
+							<a href="details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/samsung-galaxy-a71-a715.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Samsung Galaxy A71 A715</a></p>.
+						<p class="price">7.750.000₫</p>	  
+						<div class="marsk">
+							<a href="details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div> 
+				</div>                	                	
+			</div>
+
+			<div class="products">
+				<h3>sản phẩm mới</h3>
+				<div class="product-list row">
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/product-1.png" class="img-thumbnail"></a>
+						<p><a href="#">iPhone 6S Plus 64G</a></p>
+						<p class="price">10.000.000</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/product-2.png" class="img-thumbnail"></a>
+						<p><a href="#">iPhone 6S Plus 64G</a></p>
+						<p class="price">10.000.000</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/product-3.png" class="img-thumbnail"></a>
+						<p><a href="#">iPhone 6S Plus 64G</a></p>
+						<p class="price">10.000.000</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/product-4.png" class="img-thumbnail"></a>
+						<p><a href="#">iPhone 6S Plus 64G</a></p>
+						<p class="price">10.000.000</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/product-1.png" class="img-thumbnail"></a>
+						<p><a href="#">iPhone 6S Plus 64G</a></p>
+						<p class="price">10.000.000</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/product-2.png" class="img-thumbnail"></a>
+						<p><a href="#">iPhone 6S Plus 64G</a></p>
+						<p class="price">10.000.000</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/product-3.png" class="img-thumbnail"></a>
+						<p><a href="#">iPhone 6S Plus 64G</a></p>
+						<p class="price">10.000.000</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/product-4.png" class="img-thumbnail"></a>
+						<p><a href="#">iPhone 6S Plus 64G</a></p>
+						<p class="price">10.000.000</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                      
+					</div> 
+				</div>    
+			</div>
+		</div>
+		
+
+<?php 
+	include('./asset/footer.php');
+ ?>
